48469655689700af433f9127bfc6a6978469ae9d
Mohityadav-code <mohityadav3552@gmail.com>
2023-08-14 01:31:45 +0530

    iterate from index 1

--- Reduce.js
+++ Reduce.js
@@ -15,7 +15,7 @@ console.log(ObjectFromArray);
 const countriesArray = [
   {
     name: "India",
-    population: 1000,
+    population: 14000,
     capital: "Delhi",
     currency: "Rupee",
     languages: ["Hindi", "English"],
@@ -35,18 +35,46 @@ const countriesArray = [
   },
   {
     name: "France",
-    population: 1000,
+    population: 100,
     capital: "Paris",
     currency: "Euro",
     languages: ["French"],
   },
 ];
-//  * Let us first calculate total population of all countries present in this array 
+//  * Let us first calculate total population of all countries present in this array
 
-const totalPopulationn= countriesArray.reduce((popuplation,countries) =>{
-    console.log(countries+popuplation.population)
-    return popuplation+countries.population
-},0)
-console.log('totalPopulationn: ', totalPopulationn);
+const totalPopulationn = countriesArray.reduce((popuplation, countries) => {
+  console.log(countries + popuplation.population);
+  return popuplation + countries.population;
+}, 0);
+console.log("totalPopulationn: ", totalPopulationn);
 
-// here as we can see first argument is always acts as accumulator 
+// here as we can see first argument is always acts as accumulator  and we can not change their values .
+
+// ! Now we will get which country have highest population and which country have lowest population using reduce method
+
+const highestPopulation = countriesArray.reduce((acc, country) => {
+  if (acc.population < country.population) {
+    acc = country.population;
+  }
+  return acc;
+}, countriesArray[0]);
+
+console.log("highestPopulation: ", highestPopulation);
+
+//  here we have iteraterd through array of countries and we passed initial value as population of first country so it compares with second then next which ever is lower now new accumulator assigns that value and start comparing with that element
+
+const lowestPopulation = countriesArray.reduce(
+  (accumulator, country, index) => {
+    if (index > 0) {
+      console.log(accumulator, country.population);
+      if (accumulator > country.population) {
+        accumulator = country.population;
+      }
+    }
+    return accumulator;
+  },
+  countriesArray[0].population
+);
+
+console.log("lowestPopulation: ", lowestPopulation);
